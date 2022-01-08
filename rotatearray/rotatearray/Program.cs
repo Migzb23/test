bc90ac2c15bd0786960014ea8dd91946a59b28fa
Migzb23 <97329456+Migzb23@users.noreply.github.com>
2022-01-08 16:39:13 +0800

    Add files via upload

---
+++ rotatearray/rotatearray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace rotatearray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            int[] a = new int[100];
+
+            n = 3;
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            Console.Write("\n\nThe values store into the array in reverse are :\n");
+            for (i = n - 1; i >= 0; i--)
+            {
+                Console.Write("{0} ", a[i]);
+            }
+            Console.Write("\n\n");
+
+        }
+    }
+}
